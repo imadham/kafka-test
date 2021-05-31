93a5219d6c02ab8abd69b5883c720949f0c28cf3
imadham <imad.hamameh@gmail.com>
2021-05-31 16:52:25 +0300

    first producer

---
+++ src/main/java/kafka/test/tutorial1/PrudocerDemo.java
@@ -0,0 +1,32 @@
+package kafka.test.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class PrudocerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("imad_topic", "hello world");
+
+        //send data - asynchrous
+        producer.send(producerRecord);
+
+        producer.flush(); // or .colse()
+    }
+}
